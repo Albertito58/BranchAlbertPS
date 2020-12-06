f4a3c18de7ef17423275c416a8e018b1698dd1b0
Albertito58 <a.p.s.58@hotmail.com>
2020-12-06 10:32:39 +0100

    Update ExempleCommitBranch.c

--- ExempleCommitBranch.c
+++ ExempleCommitBranch.c
@@ -4,10 +4,14 @@
 void main(){
 funcio1();
 funcionalitatExtra();
+funcionalitatExtra2();
 }
 void funcio1(){
     printf("Hola");
 }
 void funcionalitatExtra(){
     printf("Que tal?")
+}
+void funcionalitatExtra2(){
+    printf("Quin fred fa.....")
 }
