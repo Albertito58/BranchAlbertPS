7b17f27beb89463daa16566f5a398375e6aa5039
Albertito58 <a.p.s.58@hotmail.com>
2020-12-06 11:02:44 +0100

    Update ExempleCommitBranch.c

--- ExempleCommitBranch.c
+++ ExempleCommitBranch.c
@@ -5,7 +5,7 @@ void main(){
 print("Hola!!!")
 funcio1();
 funcionalitatExtra();
-printf("Parece que todo va bien pero podria haber salido mal....");
+printf("Parece que todo va bien pero podria haber salido mal....No lose todavia");
 }
 void funcio1(){
     printf("Hola");
